4dae4c0f0e6b41460906c20d50468205a162ad20
suhas-malhotra <suhas-int@frigga.app>
2022-08-18 00:50:45 +0530

    comments added

--- controllers/pricing_api.js
+++ controllers/pricing_api.js
@@ -15,19 +15,28 @@ module.exports.getPrice = async (req, res) => {
     method: "GET",
     url: `https://maps.googleapis.com/maps/api/geocode/json?addressfrom=${fromAddress}&sensor=false&key=${process.env.GEO_API}&addressTo=${toAddress}`,
   };
-  const distane = await axios(options);
+  //finding distance using google api
+  const distance = await axios(options);
 
+  //Throw ERROR
+  //distance greater than 1000
   if (distance > 1000) {
     return res.status(404).json({ msg: "Distance is very much" });
   }
+  //which vichle is used 
   const vehicleCosting = Price.findOne({ vehicle }, function (err) {
     if (err) {
       return res.status(404).json({ msg: "Something Went Wrong" });
     }
   });
 
+  //Throw ERROR
+  //if no vehicle with given name is found
+  if(vehicleCosting.length == 0){
+    return res.status(404).json({ msg: "Type of Vehicle is Invalid" });
+  }
   let price = 0;
-
+  //calculation the cost 
   if (vehicleCosting.baseKms >= distance) {
     price = vehicleCosting.baseAmount;
   } else {
@@ -37,6 +46,7 @@ module.exports.getPrice = async (req, res) => {
     let restKms = distance * vehicleCosting.AmountPerKm;
     price += restKms;
   }
+  //price greater than 50 email is needed
   if (price > 50) {
     return res.status(200).json({ msg: "Email Neeeded", bool: true });
   } else {
